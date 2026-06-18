d1739584ac92f7e0f0b6db8e15d03bae7f4d4a77
Hidekazu Suzuki <HidekazuSuzuki@users.noreply.github.com>
2026-06-18 10:06:12 +0900

    ピンのアイコンをゴミ収集車の画像に変更

--- truck.js
+++ truck.js
@@ -35,16 +35,10 @@
 
   // 塵芥車アイコン（マーカーを視覚的に区別しやすくする）
   const truckIcon = L.icon({
-    iconUrl:
-      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-    iconRetinaUrl:
-      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-    shadowUrl:
-      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
+    iconUrl: "./assets/icon-truck.png",
+    iconSize: [64, 27],
+    iconAnchor: [32, 27],
+    popupAnchor: [0, -27]
   });
 
   // 現在地図上に表示している最新マーカー（塵芥車1台分=1ピン想定）
